aba0d014f788e66aa706f438cc3e3b062c553ab8
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-06-25 17:58:34 -0300

    post agregar palabra

--- src/actividad.js
+++ src/actividad.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.use(express.json())
 
-const frase = 'Esto es un texto de prueba backend, usando get con postman'
+let frase = 'Esto es un texto de prueba backend, usando get con postman'
 app.get('/api/frase', (req, res) => {
     res.json({ frase })
 })
@@ -17,8 +17,18 @@ app.get('/api/frase/:palabra', (req, res) => {
     res.json({
         encontrada: lista[palabra]
     })
+})
 
+app.post('/api/palabras', (req, res) => {
+    const { palabra } = req.body
 
+    frase += ` ${palabra}`
+    const position = frase.split(' ').length
+
+    res.json({
+        agregada: palabra,
+        position
+    })
 })
 
 app.listen(8080)
