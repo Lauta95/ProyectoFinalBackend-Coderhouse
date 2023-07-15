16aa59bc2f8be42c29f6997c4eed724d1d167988
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-07-14 21:49:48 -0300

    creador de productos con un formulario renderizando desde el front

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 import express from 'express'
 import productRouter from './routes/product.router.js'
 import cartRouter from './routes/cart.router.js'
+import viewsRouter from './routes/views.router.js'
+import handlebars from 'express-handlebars'
+import __dirname from './utils.js'
 const app = express()
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-// middleware
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+
+app.use('/static', express.static(__dirname + '/public'))
+
+app.use('/', viewsRouter)
 app.use('/api/products', productRouter)
 app.use('/api/carts', cartRouter)
 app.listen(8080)
