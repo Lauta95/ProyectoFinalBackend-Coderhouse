101c194ba5eefa23fd12e54e89b10694c291c228
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-07-15 11:21:14 -0300

    real-time

--- src/app.js
+++ src/app.js
@@ -23,5 +23,7 @@ const httpServer = app.listen(8080)
 const io = new Server(httpServer)
 
 io.on('connection', socket => {
-    
+    socket.on('new-product', data => {
+        console.log(data);
+    })
 })
