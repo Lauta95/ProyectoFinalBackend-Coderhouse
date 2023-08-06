42b91ec203130f33e0cc01c1ed22b6038b92c06e
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-08-06 18:01:45 -0300

    se agregan todos los endpoints solicitados en el cart.router

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -14,7 +14,7 @@ router.get('/:cid/', async (req, res) => {
     res.send(result)
 })
 router.post('/', async (req, res) => {
-    const result = await CartModel.create({products: []})
+    const result = await CartModel.create({ products: [] })
     res.send(result)
 })
 router.post('/:cid/product/:pid', async (req, res) => {
@@ -33,6 +33,58 @@ router.post('/:cid/product/:pid', async (req, res) => {
 
     const result = await cart.save()
     res.send(result);
+});
+
+// Nuevos endpoints de la segunda preentrega:
+
+// para actualizar el carrito por id
+router.put('/:cid', async (req, res) => {
+    const cid = req.params.cid;
+    const { products } = req.body;
+
+    const cart = await CartModel.findOne({ _id: cid });
+    cart.products = products;
+
+    const result = await cart.save();
+    res.send(result);
+});
+
+// para poder actualizar solamente la cantidad de ejemplares del producto por cualquier cantidad pasada desde el req.body:
+router.put('/:cid/products/:pid', async (req, res) => {
+    const cid = req.params.cid;
+    const pid = req.params.pid;
+    const { quantity } = req.body;
+
+    const cart = await CartModel.findOne({ _id: cid });
+
+    const existingProduct = cart.products.find((product) => product.id === pid);
+    if (existingProduct) {
+        existingProduct.quantity = parseInt(quantity, 10);
+    }
+
+    const result = await cart.save();
+    res.send(result);
+});
+
+// para borrar del carrito el producto seleccionado
+router.delete('/:cid/products/:pid', async (req, res) => {
+    const cid = req.params.cid;
+    const pid = req.params.pid;
+
+    const cart = await CartModel.findOne({ _id: cid });
+
+    cart.products = cart.products.filter((product) => product.id !== pid);
+
+    const result = await cart.save();
+    res.send(result);
+});
+
+// para borrar todos los productos del carrito
+router.delete('/:cid', async (req, res) => {
+    const cid = req.params.cid;
+
+    await CartModel.deleteOne({ _id: cid });
+    res.send({ status: 'success', message: 'Cart deleted successfully' });
 });
 
 export default router
