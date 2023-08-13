114de8c47b29b73393d1420695a055dcca6dfb12
lautaro <lautaro@debian.local.host>
2023-08-12 22:22:03 -0300

    login

--- src/sessions.js
+++ src/sessions.js
@@ -16,11 +16,49 @@ app.get('/session', (req, res) => {
 
     if (req.session.contador) {
         req.session.contador++
-        return res.send(`se ha visitado el sitio ${req.session.contador} veces`)
+        return res.send(`se ha visitado el sitio ${req.session.contador} veces  `)
     }
 
     req.session.contador = 1
     res.send('welcome!')
+})
+
+app.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) return res.send('logout error')
+
+        return res.send('logout OK')
+    })
+})
+
+const DB = [
+    {
+        username: 'lautaro',
+        password: 'secret',
+        rol: 'admin'
+    }
+]
+
+app.get('/login', (req, res) => {
+    const { username, password } = req.query
+
+    const user = DB.find(u => u.username === username && u.password === password)
+    if (!user) return res.send('invalid credentials')
+
+    req.session.user = user
+
+    res.send('login success')
+})
+
+// req.session?.user checks if the req.session object exists, and if it does, it checks if the user property exists within the req.session object. If either req.session or req.session.user is null or undefined, the expression will evaluate to undefined without throwing an error. If both req.session and req.session.user exist, the expression will evaluate to the value of req.session.user.
+function authentication(req, res, next) {
+    if(req.session?.user) return next()
+
+    return res.status(401).send('error de autenticación')
+}
+
+app.get('/private', authentication, (req, res) => {
+    res.send('esta pagina la puede ver la persona logueada ' + JSON.stringify(req.session.user))
 })
 
 app.listen(8080)
