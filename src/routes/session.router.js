a8eb805eac38501e590bbd3cdcfe3893946b70e5
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-08-15 21:38:47 -0300

    correcciones de errores en el session, y en el register

--- src/routes/session.router.js
+++ src/routes/session.router.js
@@ -4,17 +4,35 @@ import UserModel from "../DAO/mongoManager/models/user.model.js";
 const router = Router()
 
 router.post('/login', async (req, res) => {
+
     const { email, password } = req.body
     const user = await UserModel.findOne({ email, password })
-    if (!user) return res.redirect('login')
+    if (!user) {
+        console.log('hi');
+        return res.render('login', { error: 'Invalid credentials' });
+
+    }
     req.session.user = user
     return res.redirect('/profile')
 })
 
-router.post('/register', async(req, res) => {
-    const user = req.body.user
-    await UserModel.create(user)
-    return res.redirect('/')
+router.post('/register', async (req, res) => {
+    try {
+        const { first_name, last_name, email, age, password } = req.body;
+        const user = {
+            first_name,
+            last_name,
+            email,
+            age,
+            password
+        };
+        await UserModel.create(user)
+        console.log(user);
+        return res.redirect('/')
+    } catch (error) {
+        console.error('Error creating user:', error);
+        return res.render('register', { error: 'User creation failed' });
+    }
 })
 
 export default router
