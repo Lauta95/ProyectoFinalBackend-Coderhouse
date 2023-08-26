f4e51cfc2e7062cd4d658267509d36aa1072172c
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-08-26 13:13:21 -0300

    google strat

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -5,43 +5,45 @@ import { createHash, isValidPassword } from '../utils.js';
 import passportGoogle from 'passport-google-oauth20'
 import GitHubStrategy from 'passport-github2'
 
+// GH->
 // App ID: 375160
 // Client ID: Iv1.270c5a68790735e4
 // secret: 9d675ea5525ade24d354e66a9866417d85fb9991
 
 const LocalStrategy = local.Strategy
-// const GoogleStrategy = passportGoogle.Strategy;
+const GoogleStrategy = passportGoogle.Strategy;
 
-// const GOOGLE_CLIENT_ID = ''
-// const GOOGLE_CLIENT_SECRET = ''
+const GOOGLE_CLIENT_ID = '719876990121-rp7993d5rjb2c8p52oeaavpa2sungap3.apps.googleusercontent.com'
+const GOOGLE_CLIENT_SECRET = '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'
+
+const initializePassport = () => {
+
 
     // los LocalStrategy funcionan como un middleware
 
-    // passport.use(new GoogleStrategy({
-    //     clientID: GOOGLE_CLIENT_ID,
-    //     clientSecret: GOOGLE_CLIENT_SECRET,
-    //     callbackURL: "http://127.0.0.1:8080/callback-google"
-    //   },
-    //   async function(accessToken, refreshToken, profile, done) => {
-    //     const email = profile.emails[0].value
-    //     UserModel.findOrCreate({ email }, (err, user) => {
-    //       return done(err, user);
-    //     });
-    //   }
-    // ))
-const initializePassport = () => {
-
+    passport.use(new GoogleStrategy({
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://127.0.0.1:8080/api/session/callback-google"
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        const email = profile.emails[0].value
+        UserModel.findOrCreate({ email }, (err, user) => {
+          return done(err, user);
+        });
+      }
+    ))
 
     passport.use('github', new GitHubStrategy(
         {
             clientID: 'Iv1.270c5a68790735e4',
             clientSecret: '9d675ea5525ade24d354e66a9866417d85fb9991',
-            callbackURL:'http://127.0.0.1:8080/api/session/githubcallback'
+            callbackURL: 'http://127.0.0.1:8080/api/session/githubcallback'
         },
         async (accessToken, refreshToken, profile, done) => {
-            try{
-                const user = await UserModel.findOne({email: profile._json.email})
-                if(user) {
+            try {
+                const user = await UserModel.findOne({ email: profile._json.email })
+                if (user) {
                     console.log('user already exists ' + profile._json.email);
                     return done(null, user)
                 }
@@ -52,7 +54,7 @@ const initializePassport = () => {
                 }
                 const result = await UserModel.create(newUser)
                 return done(null, result)
-            } catch(e) {
+            } catch (e) {
                 return done('error to login with github' + e)
             }
         }
