cc2a6507ba5f1703eafcfd8e0608f9d7bed472f6
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-09-27 23:56:03 -0300

    implementacion de ticket falta mostrar

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { find, findOne, create, findOneBody, pidBody, productDetails, deleteCart, deleteAll } from "../controllers/cart.controller.js"
+import { cartPurchase } from "../controllers/ticket.controller.js"
 
 const router = Router()
 
@@ -11,5 +12,6 @@ router.put(':cid/products/pid', pidBody)
 router.post(':cid/products:pid', productDetails)
 router.delete('/:cid/products/:pid', deleteCart)
 router.delete('/:cid', deleteAll)
+router.post("/:cid/purchase", cartPurchase)
 
 export default router
